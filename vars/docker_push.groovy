e791677326fae1b671422b71b4492ad6cc122513
Killua93 <129741790+Killua93@users.noreply.github.com>
2024-09-02 22:40:36 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,6 +1,6 @@
 def call(String dockerhubusr, String Docker_Image, String Docker_Tag) {
-   withCreadentials([usernamePassword(credentialsId: "docker_creds" , passwordVariables: "dockerhubpass", usernameVariable: "dockerhubusr" )]) {
-       sh " docker login -u ${dockerhubusr} -p ${dockerhubpass}"
+   withCredentials([usernamePassword(credentialsId: "docker_creds" , passwordVariable: "dockerhubpass", usernameVariable: "dockerhubusr" )]) {
+       sh "docker login -u ${dockerhubusr} -p ${dockerhubpass}"
                                       }
    sh "docker push ${dockerhubusr}/${Docker_Image}:${Docker_Tag}"
 }
