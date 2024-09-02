6781701afa7a39e221e63f74d4a484cfb4d6b34e
Killua93 <129741790+Killua93@users.noreply.github.com>
2024-09-02 23:45:34 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,3 +1,3 @@
 def call(String DockerHub_User, String Docker_Image, String Docker_Tag) {
-    sh "sudo docker build -t ${DockerHub_User}/${Docker_Image}:${Docker_Tag} . "
+    sh "docker build -t ${DockerHub_User}/${Docker_Image}:${Docker_Tag} . "
 }
